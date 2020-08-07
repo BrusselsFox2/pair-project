947568c5d3474192d1e6e994dce4889089953a67
nurinhidayatullah <n.hidayatullah94@gmail.com>
2020-08-07 09:53:20 +0700

    add search function API TvMaze

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router()
 
 const movieRoute = require('./movie-route')
 const userRouter = require('./user')
+const tvMazeRoute = require('./tvMaze')
 
 router.use('/users', userRouter)
 router.use('/movies',movieRoute)
+router.use('/search', tvMazeRoute)
+
 
 module.exports = router
